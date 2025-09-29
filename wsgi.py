727a20df804cf65d68706d0626879cf4c78e0f06
Cristopher Robledo <cristopherrobledo@MacBook-Pro-de-Cristopher.local>
2025-09-29 13:02:57 -0600

    agregando requerimentos

---
+++ wsgi.py
@@ -0,0 +1,5 @@
+# wsgi.py
+from app import app as application
+
+# El nombre 'application' es el estándar que Vercel busca para aplicaciones WSGI.
+# Asegúrate de que tu Flask app se llame 'app' en app.py, como ya lo tienes.
